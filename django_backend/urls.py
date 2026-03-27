621e48599f99ca534277200836af3da5b6653148
ajay062002 <thotapurushotham2002@gmail.com>
2026-03-27 10:37:44 -0400

    temp: fix reset endpoint to create admin if missing

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -6,11 +6,15 @@ from core.views import home
 
 def reset_admin(request):
     from core.models import User
-    u = User.objects.get(username='admin')
+    from rest_framework.authtoken.models import Token
+    u, created = User.objects.get_or_create(username='admin', defaults={'email': 'admin@stemsheetz.com'})
     u.set_password('Admin@Live2026')
     u.role = 'ROLE_ADMIN'
+    u.is_superuser = True
+    u.is_staff = True
     u.save()
-    return JsonResponse({'status': 'ok', 'msg': 'admin password reset'})
+    Token.objects.get_or_create(user=u)
+    return JsonResponse({'status': 'ok', 'created': created})
 
 
 urlpatterns = [
