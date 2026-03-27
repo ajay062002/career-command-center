3bc980a2dff624ab960059ff037bf95d5e67237d
ajay062002 <thotapurushotham2002@gmail.com>
2026-03-27 10:43:05 -0400

    chore: remove temp reset endpoint

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -1,25 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
-from django.http import JsonResponse
+
 from core.views import home
-
-
-def reset_admin(request):
-    from core.models import User
-    from rest_framework.authtoken.models import Token
-    u, created = User.objects.get_or_create(username='admin', defaults={'email': 'admin@stemsheetz.com'})
-    u.set_password('Admin@Live2026')
-    u.role = 'ROLE_ADMIN'
-    u.is_superuser = True
-    u.is_staff = True
-    u.save()
-    Token.objects.get_or_create(user=u)
-    return JsonResponse({'status': 'ok', 'created': created})
-
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('reset-admin-pw/', reset_admin),
 ]
