b40d1ce7991aa80a10841f2a7c8d7b0c4ee7a272
ajay062002 <thotapurushotham2002@gmail.com>
2026-03-27 10:31:49 -0400

    temp: reset admin password endpoint

--- django_backend/urls.py
+++ django_backend/urls.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
 from django.urls import path, include
-
+from django.http import JsonResponse
 from core.views import home
+
+
+def reset_admin(request):
+    from core.models import User
+    u = User.objects.get(username='admin')
+    u.set_password('Admin@Live2026')
+    u.role = 'ROLE_ADMIN'
+    u.save()
+    return JsonResponse({'status': 'ok', 'msg': 'admin password reset'})
+
 
 urlpatterns = [
     path('', home),
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
+    path('reset-admin-pw/', reset_admin),
 ]
